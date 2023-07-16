fb84406d87c64ab6e7d539d0bd6bae4e97488d18
allaneuscate <allaneuscate@hotmail.com>
2023-07-16 15:11:42 -0300

    Migra HTML do componente inicial

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import Home from "./components/pages/Home"
+import Home from "./components/pages/Home";
 
-import "./styles/normalize.css"
-import "./styles/fontawesome.min.css"
-import "./styles/main.css"
+import "./styles/normalize.css";
+import "./styles/fontawesome.min.css";
+import "./styles/main.css";
 
-function App() {
-  return <Home />;
+function App() { return <Home />;
 }
 
 export default App;
